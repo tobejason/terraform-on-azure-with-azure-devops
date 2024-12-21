98c857d1cda23bf1aa99cd176ec506cd6c427a57
Chukwuemeka Peter <154003938+tobejason@users.noreply.github.com>
2024-12-21 00:18:27 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,14 @@
+- Define Input Variables for VM Size and VM admin user name. 
+- If we required we can parameterize more arguments in `azurerm_linux_virtual_machine` resource. 
 
+# Linux VM Input Variables Placeholder file.
+variable "web_linuxvm_size" {
+ description = "Web Linux VM Size"
+ type = string 
+ default = "Standard_DS1_v2"
+}
+variable "web_linuxvm_admin_user" {
+ description = "Web Linux VM Admin Username"
+ type = string 
+ default = "azureuser"
+}
